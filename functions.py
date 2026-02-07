26c81495d2079faac1d3cd7aa75e99a1fed66da7
lucasTamborelli <lukastamborelli@gmail.com>
2026-02-07 12:56:56 -0300

    Mais documentos IPR
    Streamlit Feedback feature

---
+++ functions.py
@@ -0,0 +1,132 @@
+from pinecone import Pinecone
+from collections import defaultdict
+from dotenv import load_dotenv
+import os
+import csv
+import streamlit as sl
+
+load_dotenv()
+keyPinecone = os.getenv("API_KEY_PINECONE")
+
+def semantic_search(index_name, text_chunks, query, top_k, printer):
+
+    pc = Pinecone(api_key= keyPinecone)
+    if not pc.has_index(index_name):
+        pc.create_index_for_model(
+            name=index_name,
+            cloud = 'aws',
+            region = 'us-east-1',
+            embed = {
+                'model':'llama-text-embed-v2',
+                'field_map':{'text': 'chunk_text'}
+            }
+        )
+    records = [
+        {
+            "_id": f"chunk_{i+1}",
+            "chunk_text": chunk,
+            "category": "pdf_content"
+        }
+        for i, chunk in enumerate(text_chunks)
+    ]
+    dense_index = pc.Index(index_name)
+    dense_index.upsert_records('teste1', records)
+    results = dense_index.search(
+        namespace="teste1",
+        query={
+            "top_k": top_k,
+            "inputs": {
+                'text': query
+            }
+        }
+    )
+    context_texts = []
+    for hit in results['result']['hits']:
+            if printer:
+                print(f"id: {hit['_id']:<5} | score: {round(hit['_score'], 2):<5} | category: {hit['fields']['category']:<10} | text: {hit['fields']['chunk_text']:<50}")     
+            context_texts.append(hit['fields']['chunk_text'])
+
+    # dense_context = "\n\n".join(context_texts)
+    # return dense_context
+    return results  # <-- Return the full results dict
+
+
+def keyword_search(index_name, text_chunks, query, top_k, printer):
+    
+    pc = Pinecone(api_key= keyPinecone)
+    if not pc.has_index(index_name):
+        pc.create_index_for_model(
+            name=index_name,
+            cloud="aws",
+            region="us-east-1",
+            # metric = "dotproduct",
+            embed={
+                "model": "pinecone-sparse-english-v0",
+                "field_map": {"text": "chunk_text"}
+            }
+        )
+    records = [
+        {
+            "_id": f"chunk_{i+1}",
+            "chunk_text": chunk,
+            "category": "pdf_content"
+        }
+        for i, chunk in enumerate(text_chunks)
+    ]
+    sparse_index = pc.Index(index_name)
+    sparse_index.upsert_records(namespace="pdf", records=records)
+    results = sparse_index.search(
+        namespace="pdf",
+        query={
+            "top_k": top_k,
+            "inputs": {"text": query}
+        }
+    )
+    context_texts = []
+    for hit in results["result"]["hits"]:
+        if printer:
+            print(f"id: {hit['_id']:<5} | score: {round(hit['_score'], 2):<5} | category: {hit['fields']['category']:<10} | text: {hit['fields']['chunk_text']:<50}")    
+    
+    # sparse_context = "\n\n".join(context_texts)
+    # return sparse_context
+    return results  
+
+
+
+def hybrid_search(dense_result, sparse_result, sparse_weight, top_k, printer):
+    final_score = defaultdict(lambda: {"text": "", "dense": 0, "sparse": 0}) 
+
+    for hit in dense_result["result"]["hits"]:
+        _id = hit["_id"]
+        final_score[_id]["text"] = hit["fields"]["chunk_text"]
+        final_score[_id]["dense"] = hit["_score"]    
+
+
+    for hit in sparse_result["result"]["hits"]:
+        _id = hit["_id"]
+        final_score[_id]["text"] = hit["fields"]["chunk_text"]
+        final_score[_id]["sparse"] = hit["_score"]
+
+
+    final = []
+    for _id, vals in final_score.items():
+        combined_score = sparse_weight * vals["sparse"] + (1 - sparse_weight) * vals["dense"]
+        final.append({"_id": _id, "text": vals["text"], "score": combined_score})
+
+    final = sorted(final, key=lambda x: x["score"], reverse=True)
+    top_k_final = final[:top_k]
+    
+    for hit in top_k_final:
+        if printer: 
+            print(f"id: {hit['_id']} | score: {hit['score']:.2f} | text: {hit['text'][:500]}...")
+    return top_k_final
+
+
+def save_feedback(arq, query, answer):
+    score = sl.session_state.get('avaliacao_widget')
+    with open(arq, mode='a', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        if not os.path.isfile(arq):
+            writer.writerow(['Pergunta', 'Resposta', 'Avaliacao'])
+        writer.writerow([query, answer, score])
+    print(f"Answers saved {score}")
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from classes import *
+from functions import *
+import streamlit as sl
+import glob
+
+
 
 if __name__ == "__main__":
     
@@ -13,12 +18,34 @@ if __name__ == "__main__":
 
     llm = LLM_cloud('gpt-oss:120b-cloud', temperature = 0.1)
 
-    sl.header("Chat infos IPR")
+
+
+
+    if 'resposta_llm' not in sl.session_state: sl.session_state.resposta_llm = None
+    if 'ultima_query' not in sl.session_state: sl.session_state.ultima_query = ""
+
     query = sl.text_input(label='Pergunta:')
-    if query: 
-        semantic_context = semantic_search(index_name='teste', text_chunks=chunks, query=query, top_k = 5, printer= False) # 1000, 200
-        keyword_context = keyword_search(index_name='teste22', text_chunks=chunks, query=query, top_k = 5, printer= False) # 1000, 400
-        # sparce == samantic -> 60% semantic
-        hybrid_context = hybrid_search(keyword_context, semantic_context, sparse_weight = 0.6, top_k = 3, printer = False)
-        answer = llm.response(query, hybrid_context)
+    buscar = sl.button('Buscar') 
+    if buscar or (query and query != sl.session_state.ultima_query):
+        semanticCtx = semantic_search(index_name='teste', text_chunks=chunks, query=query, top_k = 5, printer= False) # 1000, 200
+        keywordCtx = keyword_search(index_name='teste22', text_chunks=chunks, query=query, top_k = 5, printer= False) # 1000, 400
+        # sparce == semantic -> 60% semantic
+        hybridCtx = hybrid_search(keywordCtx, semanticCtx, sparse_weight = 0.6, top_k = 3, printer = False)
+        answer = llm.response(query, hybridCtx)
         sl.write(answer)
+
+        sl.session_state.resposta_llm = answer
+        sl.session_state.ultima_query = query
+
+
+        if sl.session_state.resposta_llm:
+            sl.write(sl.session_state.resposta_llm)
+            sl.write("---") 
+
+            evaluation = sl.radio( 'Avaliação da resposta:', ("Ruim", "Médio", "Bom"),
+                key='avaliacao_widget',
+                on_change=save_feedback,
+                index=None,
+                args=('feedback.csv', query, answer),
+            )
+            
